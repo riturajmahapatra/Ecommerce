a42ad9cd68324d47e6c745bfbfecff04a435b434
riturajmahapatra <riturajmahapatra@gmail.com>
2023-10-09 16:55:14 +0530

    page setup

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <>
+      herobanner
+      <div className=""></div>
+    </>
+  );
 };
 
 export default page;
